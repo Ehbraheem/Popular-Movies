e87d295313db2269134ff9370fb113c46e343b60
Ehbraheem <ehbraheem@gmail.com>
2017-04-07 16:42:00 +0100

    Complete all functionality

--- app/src/main/java/com/example/android/popularmovies/data/Movies.java
+++ app/src/main/java/com/example/android/popularmovies/data/Movies.java
@@ -1,5 +1,8 @@
 package com.example.android.popularmovies.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -7,7 +10,7 @@ import org.json.JSONObject;
  * Created by Ehbraheem on 05/04/2017.
  */
 
-public class Movies {
+public class Movies implements Parcelable {
 
     public String title;
     public String posterUrl;
@@ -27,5 +30,47 @@ public class Movies {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeStringArray(new String[] {this.title,
+                                            this.plot,
+                                            this.posterUrl,
+                                            this.releaseDate,
+                                            this.rating
+        });
+
+    }
+
+    public static final Parcelable.Creator<Movies> CREATOR
+            = new Creator<Movies>() {
+        @Override
+        public Movies createFromParcel(Parcel source) {
+            return new Movies(source);
+        }
+
+        @Override
+        public Movies[] newArray(int size) {
+            return new Movies[size];
+        }
+    };
+
+    private Movies(Parcel source){
+        String[] data = new String[5];
+
+        source.readStringArray(data);
+
+        this.title        = data[0];
+        this.plot         = data[1];
+        this.posterUrl    = data[2];
+        this.releaseDate  = data[3];
+        this.rating       = data[4];
     }
 }
--- app/src/main/java/com/example/android/popularmovies/utils/MoviesAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/utils/MoviesAdapter.java
@@ -11,11 +11,11 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.android.popularmovies.APIDetails;
+import com.example.android.popularmovies.MainActivity;
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.data.Movies;
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
 
 
 /**
@@ -26,15 +26,23 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
 
     private static final String TAG = MoviesAdapter.class.getSimpleName();
 
+    private ListItemClickListener mOnclickListener;
+
     //    private static int viewHolderCout;
     private Context mContext;
+
+    public interface ListItemClickListener {
+
+        void onListItemClick(int itemIndex, Movies movie);
+    }
 //
 //    private int nmNumberItems;
     private Movies[] movies;
 
-    public MoviesAdapter(Movies[] movies, Context context) {
+    public MoviesAdapter(Movies[] movies, MainActivity activity) {
         this.movies = movies;
-        this.mContext = context;
+        this.mContext = activity;
+        this.mOnclickListener = activity;
     }
 
     @Override
@@ -62,13 +70,13 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         return movieViewHolder;
     }
 
-    class MovieViewHolder extends RecyclerView.ViewHolder {
+    class MovieViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
 
-        TextView mTitle;
-        ImageView mPosterImage;
-//        final TextView mPlot;
-//        final TextView mReleaseDate;
-//        final RatingBar mUserRating;
+        final TextView mTitle;
+        final ImageView mPosterImage;
+
+        final RatingBar mUserRating;
 
         public MovieViewHolder(View itemView) {
 
@@ -76,13 +84,32 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
 
             this.mTitle = (TextView) itemView.findViewById(R.id.movies_title);
             this.mPosterImage = (ImageView) itemView.findViewById(R.id.movies_thumbnail);
+            this.mUserRating = (RatingBar) itemView.findViewById(R.id.movie_rating);
+
+            itemView.setOnClickListener(this);
 //            this.mPlot = (RatingBar) itemView.findViewById(R.id.movies_title);
         }
 
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnclickListener.onListItemClick(clickedPosition, movies[clickedPosition]);
+        }
+
         void bind(Movies movie, Context context) {
+
+            String imageSize = "w185/";
+
             mTitle.setText(movie.title);
+
+            float rating = Float.parseFloat(movie.rating) / 2;
+
+            mUserRating.setNumStars(5);
+            mUserRating.setStepSize(0.5f);
+            mUserRating.setRating(rating);
+
             Picasso.with(context)
-                    .load(String.valueOf(APIDetails.makePosterUrl(movie.posterUrl)))
+                    .load(String.valueOf(APIDetails.makePosterUrl(movie.posterUrl, imageSize )))
                     .into(mPosterImage);
         }
     }
